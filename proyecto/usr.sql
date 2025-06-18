5b7ec657e6443eb83474823e9adf6671b0adf107
RuthlessMercy307 <121249384+RuthlessMercy307@users.noreply.github.com>
2025-06-17 22:03:57 -0300

    Add admin page and dynamic menu

--- proyecto/usr.sql
+++ proyecto/usr.sql
@@ -33,3 +33,21 @@ VALUES ('usuario_teste', 'teste@teste.com', '12345678900', '11999999999',
 
 
 
+
+CREATE TABLE pizzas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    imagem VARCHAR(255) NOT NULL
+);
+
+INSERT INTO pizzas (nome, descricao, preco, imagem) VALUES
+('5 queijos', 'Molho de tomate especial, mussarela ralada, provolone, parmesão, catupiry, cheddar, oregano e azeitonas', 37.00, '5_queijos.png'),
+('3 Porquinhos', 'Molho de tomate especial, mussarela, lombo canadense, calabresa fatiada, bacon, oregano e azeitonas', 37.00, '3_porcos.png'),
+('Bauru', 'Molho de tomate especial, mussarela ralada, coxão mole em tiras, tomate em rodelas, orégano e azeitonas', 40.00, 'bauru.png'),
+('Brocolis Especial', 'Molho de tomate especial, mussarela ralada, Brocolis refogado, bacon em cubos, catupiry, alho frito, orégano e azeitonas', 40.00, 'brocolis.png'),
+('California', 'Molho de tomate especial, mussarela ralada, calabresa fatiada, abacaxi cortado, catupiry, orégano e azeitonas', 40.00, 'california.png');
+
+INSERT INTO usuarios (cpf, usuario, telefone, email, senha)
+VALUES ('00000000000', 'admin', '11999999999', 'admin@pizza.com', '$2b$12$E2Heh4DCP29xkGGRMCxuRez62Ne7yRg.r.QoOmt.2MRriyqF/wU56');
